3023a6b9b1a4a50b0b3a5ba53edb3e5de5577845
Joshua Moses <59664518+joshuanmoses@users.noreply.github.com>
2020-07-21 16:52:00 -0500

    Create stream-cipher.sh

---
+++ stream-cipher.sh
@@ -0,0 +1,57 @@
+
+source ./askey.sh                                          # <1>
+
+#
+# Ncrypt - Encrypt - reads in characters
+#           outputs 2digit hex #s
+#
+function Ncrypt ()                                         # <2>
+{
+    TXT="$1"
+    for((i=0; i< ${#TXT}; i++))                            # <3>
+    do
+	CHAR="${TXT:i:1}"                                  # <4>
+	RAW=$(asnum "$CHAR") # " " needed for space (32)   # <5>
+	NUM=${RANDOM}
+	COD=$(( RAW ^ ( NUM & 0x7F )))                     # <6>
+	printf "%02X" "$COD"                               # <7>
+    done
+    echo						   # <8>
+}
+
+#
+# Dcrypt - DECRYPT - reads in a 2digit hex #s
+#           outputs characters
+#
+function Dcrypt ()                                  # <9>
+{
+    TXT="$1"
+    for((i=0; i< ${#TXT}; i=i+2))                   # <10>
+    do
+	CHAR="0x${TXT:i:2}"                         # <11>
+	RAW=$(( $CHAR ))                            # <12>
+	NUM=${RANDOM}
+	COD=$(( RAW ^ ( NUM & 0x7F )))              # <13>
+	aschar "$COD"                               # <14>
+    done
+    echo
+}
+
+if [[ -n $1  &&  $1 == "-d" ]]                      # <15>
+then
+    DECRYPT="YES" 
+    shift                                           # <16>
+fi
+
+KEY=${1:-1776}                                      # <17>
+RANDOM="${KEY}"                                     # <18>
+while read -r                                       # <19>
+do
+    if [[ -z $DECRYPT ]]	                    # <20>
+    then 
+	Ncrypt "$REPLY"
+    else
+	Dcrypt "$REPLY"
+    fi
+
+done
